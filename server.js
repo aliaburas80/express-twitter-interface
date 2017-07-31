e24a9961fe1d3cdcddf1be1af5ebade5e43b9d85
AliAbuRas <alaburas@mbc.net>
2017-07-31 15:15:04 +0300

    -Add Bootstrap -Style the form page

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ let ejs     = require('ejs')
 //let cookieParser = require('cookie-parser');
 let expressValidator = require('express-validator');
 let bodyParser = require('body-parser');
+
+app.use(express.static((__dirname, 'views/')));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 // in latest body-parser use like below.
@@ -19,21 +22,26 @@ app.get('/', (req,res)=>{
   res.send('hello word');
 });
 
-
-app.get('/form',function(req,res){
-   res.render('form')
+app.get('/form',(req,res)=>{
+   res.render('form');
 });
 
-app.post('/form',function(req,res){
-   console.log(req.body) //you will get your data in this as object.
-   res.send('post');
+app.post('/form',(req,res)=>{
+   console.log(req.body);
+    req.checkBody('ckey'     , 'Invalid name').notEmpty().isAlpha();
+    req.checkBody('cSecret'  , 'Invalid age').notEmpty().isAlpha();
+    req.checkBody('atSecret' , 'Invalid age').notEmpty().isAlpha();
+    req.checkBody('at'       , 'Invalid age').notEmpty().isAlpha();
+    var errors =  req.getValidationResult();
+    if (errors) {
+        console.log(errors);
+    }
+    else {
+        res.send('post');
+    }
 });
 
-
-// app.use(express.static(__dirname + '/src'));
-console.log(__dirname+'/views/form.ejs');
 //app.use('/form',express.static(__dirname+'/views/form'));
-
 app.listen(3000,function(){
   console.log('Server start at port 3000');
 });
@@ -41,19 +49,11 @@ app.listen(3000,function(){
 
 /*
 Validation
-req.checkBody('name', 'Invalid name').isAlpha();
-req.checkBody('age', 'Invalid age').notEmpty().isInt();
 
 
 To run the validation we call req.validationErrors(). This returns an array of error objects (or false if there are no errors) and is typically used like this
 
-var errors = req.validationErrors();
-if (errors) {
-    // Render the form using error information
-}
-else {
-   // There are no errors so perform action with valid data (e.g. save record).
-}
+
 
 example of error {param: 'name', msg: 'Invalid name', value: '<received input>'}
 
