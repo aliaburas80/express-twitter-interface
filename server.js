5597548f8a645d395e5bc96b7384d9f76227903a
Ali Abu Ras <aliaburas80@gmail.com>
2017-07-31 21:51:53 +0300

    work with twitter service

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 let express =require('express');
 let app     = express();
 let ejs     = require('ejs')
-
-//let cookieParser = require('cookie-parser');
 let expressValidator = require('express-validator');
 let bodyParser = require('body-parser');
+let twitterValues = {};
+
 
 app.use(express.static((__dirname, 'views/')));
-
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded());
-// in latest body-parser use like below.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator()); // Add this after the bodyParser middlewares!
-
 app.set('view engine', 'ejs');
-
 
 
 app.get('/', (req,res)=>{
@@ -27,39 +22,31 @@ app.get('/form',(req,res)=>{
 });
 
 app.post('/form',(req,res)=>{
-    // req.checkBody('ckey'     , 'Invalid name').notEmpty().isAlpha();
-    // req.checkBody('cSecret'  , 'Invalid age').notEmpty().isAlpha();
-    // req.checkBody('atSecret' , 'Invalid age').notEmpty().isAlpha();
-    // req.checkBody('at'       , 'Invalid age').notEmpty().isAlpha();
-  //  res.setHeader('Content-Type', 'application/json');
-   // console.log('<-ALI->');
-  console.log(JSON.stringify(req.body));
+   twitterValues = {
+     ckey    :req.body.ckey,
+     cSecret :req.body.cSecret,
+     atSecret:req.body.atSecret,
+     at      :req.body.at
+   }
+
+   let tw = require('twitter-service');
+   tw({
+       "twitter":[{
+         "consumer_key"       :twitterValues.ckey,
+         "consumer_secret"    :twitterValues.cSecret,
+         "access_token"       :twitterValues.at,
+         "access_token_secret":twitterValues.atSecret
+       }],
+       "links" :['http://tinyurl.com/y9keugnf'],
+       "hashtags":'#hashTags',
+       "message" :" "
+   });
+
+
+   console.log(JSON.stringify(twitterValues));
    res.render('twitterMessge')
-
-
-//{"ckey":"ergfddsaf","cSecret":"adfadsf","atSecret":"asdadsff","at":"asadff","submit":""} // send these to init for twitter server to start
-
 });
 
-//app.use('/form',express.static(__dirname+'/views/form'));
 app.listen(3000,function(){
   console.log('Server start at port 3000');
 });
-
-
-/*
-Validation
-
-
-To run the validation we call req.validationErrors(). This returns an array of error objects (or false if there are no errors) and is typically used like this
-
-
-
-example of error {param: 'name', msg: 'Invalid name', value: '<received input>'}
-
-
-
-
-
-
-*/
